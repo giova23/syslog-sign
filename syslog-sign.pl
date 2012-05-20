43d97221e4c42d819e993c1c61f78f28c30f8627
Faglioni Giovanni <giova@faglioni.it>
2012-05-20 12:39:15 +0200

    startup line restored

--- syslog-sign.pl
+++ syslog-sign.pl
@@ -19,7 +19,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
 $|=1;
-$rcsid='$Id: syslog-sign.pl,v 0.59 2012/05/20 10:35:59 giova Exp giova $';
+$rcsid='$Id: syslog-sign.pl,v 0.60 2012/05/20 10:38:39 giova Exp giova $';
 
 use POSIX;
 use MIME::Base64;
@@ -319,7 +319,7 @@ while (chomp ($line=<LOG>)) {
         print "$line\n";
 
 	# FIXME: put an "LAST RSID,GBC" reader here.
-	# <38>1 2012-05-06T04:03:37+02:00 ast syslog-sign.pl 28731 - - Starting '$Id: syslog-sign.pl,v 0.59 2012/05/20 10:35:59 giova Exp giova $' LAST RSID="1335979220" GBC="1292" NEW RSID="1336269817" GBC="0" 
+	# <38>1 2012-05-06T04:03:37+02:00 ast syslog-sign.pl 28731 - - Starting '$Id: syslog-sign.pl,v 0.60 2012/05/20 10:38:39 giova Exp giova $' LAST RSID="1335979220" GBC="1292" NEW RSID="1336269817" GBC="0" 
 
 	if ($line =~ /.*syslog-sign.pl [0-9]+ - - Starting '(.*)' LAST RSID=\"([0-9]+)\" GBC=\"([0-9]+)\".*/ ) {
 		$version="$1";
@@ -419,9 +419,9 @@ $last_rsid_gbc=<RSID>;
 chop $last_rsid_gbc;
 close(RSID);
 
-#openlog("syslog-sign.pl", "ndelay,pid", "auth");
-#syslog(LOG_INFO, "Starting '$rcsid' LAST $last_rsid_gbc NEW RSID=\"$rsid\" GBC=\"$gbc\"" ); 
-#closelog;
+openlog("syslog-sign.pl", "ndelay,pid", "auth");
+syslog(LOG_INFO, "Starting '$rcsid' LAST $last_rsid_gbc NEW RSID=\"$rsid\" GBC=\"$gbc\"" ); 
+closelog;
 
 $continue=1;
 while ($continue) {
